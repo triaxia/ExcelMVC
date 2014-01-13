6c4b4b3aaa6e79335bfcfcf60a66a29c7ab607c3
Peter Gu <pete.gu@optusnet.com.au>
2014-01-13 21:08:40 +1100

    Updated ExcelMvc

--- ExcelMvc/ExcelMvc/Sample.Application/Dna/Start.cmd
+++ ExcelMvc/ExcelMvc/Sample.Application/Dna/Start.cmd
@@ -1,6 +1,11 @@
 REM This command launches Excel with the Forbes2000.xlsx and ExcelMvc.AddinDna.xll
 
 pushd "%~dp0"
+
+if exist ".\PostBuild.cmd" (
 START Excel "..\bin\Debug\Dna\ExcelMvc.AddinDna-packed.xll" "..\bin\Debug\Dna\Forbes2000.xlsx""
+) else (
+START Excel "ExcelMvc.AddinDna-packed.xll" "Forbes2000.xlsx""
+)
 
 popd
