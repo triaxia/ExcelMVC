964db6392744a0de82f07dbea950fcdc6b0d4296
Wolfgang Stamm <wstamm@users.sf.net>
2014-05-21 11:38:27 +0200

    Added basic nuspec file and clean deprecated nuget packages in output dir

--- ExcelMvc/ExcelMvc/ExcelMvc/Properties/AssemblyInfo.cs
+++ ExcelMvc/ExcelMvc/ExcelMvc/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExcelMvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Writing Excel applications in .NET using the MVC/MVVM pattern")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExcelMvc")]
