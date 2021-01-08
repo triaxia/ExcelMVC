3ade055f89e0bb44637296f16021e2327b654c25
peter gu <peter.gu@nab.com.au>
2021-01-08 13:17:43 +1100

    Test

---
+++ Tools/TestLib/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestLib
+{
+    public class TestClass
+    {
+        public string Hello()
+        {
+            return XX();
+        }
+
+        private string XX()
+        {
+            return $"{DateTime.Now:O}";
+        }
+    }
+}
