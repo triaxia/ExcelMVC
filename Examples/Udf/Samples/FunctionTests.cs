a3ceb2db084dab1e53cd9e77fbc666438464dbcd
Peter Gu <pete.x.gu@gmail.com>
2023-08-25 09:16:41 +1000

    Mapping Rtd servers

---
+++ Examples/Udf/Samples/FunctionTests.cs
@@ -0,0 +1,37 @@
+﻿using ExcelMvc.Functions;
+using Function.Definitions;
+
+namespace Samples
+{
+    public static class FunctionTests
+    {
+        [Function(Name = "uAdd2", IsAsync = true, IsThreadSafe = false, Description = "Add 2 numbers", HelpTopic = "https://www.microsoft.com")]
+        public static double Add2(
+            [Argument(Name = "v1", Description = "argument 1")] double v1,
+            [Argument(Name = "v2", Description = "argument 2")] double v2)
+        {
+            return v1 + v2;
+        }
+
+        [Function(Name = "uAdd3", IsAsync = false, IsThreadSafe = false, Description = "Add 2 numbers", HelpTopic = "https://www.microsoft.com")]
+        public static double Add3(
+            [Argument(Name = "v1", Description = "argument 1")] double v1,
+            [Argument(Name = "v2", Description = "argument 2")] double v2,
+            [Argument(Name = "v3", Description = "argument 3")] double v3)
+        {
+            return v1 + v2 + v3;
+        }
+
+        [Function(Name = "uArg", IsAsync = false, IsThreadSafe = false, Description = "Returns Args", HelpTopic = "https://www.microsoft.com")]
+        public static object uFeed(object value)
+        {
+            return value;
+        }
+
+        [Function(Name = "uRtd", IsAsync = false, IsThreadSafe = false, Description = "Timer Rtd", HelpTopic = "https://www.microsoft.com")]
+        public static object uRtd()
+        {
+            return FunctionExecution.ExecuteRtd();
+        }
+    }
+}
