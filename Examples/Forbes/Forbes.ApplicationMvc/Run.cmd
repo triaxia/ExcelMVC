279c65a757087728819b671035a7ee6dba928507
Peter Gu <pete.x.gu@gmail.com>
2024-04-26 18:38:55 +1000

    All samples are working

--- Examples/Forbes/Forbes.ApplicationMvc/Run.cmd
+++ Examples/Forbes/Forbes.ApplicationMvc/Run.cmd
@@ -1,7 +1,7 @@
 pushd "%~dp0"
 
-REM set addin=ExcelMvc.Addin.x86.xll
-set addin=ExcelMvc.Addin.x64.xll
+REM set addin=Forbes.ApplicationMvc.xll
+set addin=Forbes.ApplicationMvc.x64.xll
 
 REM Use full path for Workbook argments. ExcelMvc gets upset with relative Workbook
 REM paths... 
