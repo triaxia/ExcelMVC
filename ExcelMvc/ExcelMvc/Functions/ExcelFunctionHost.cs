bcce9f23bf2c94160566de2bc5d85f4054035904
Peter Gu <Pete.x.gu@gmail.com>
2024-07-24 19:21:21 +1000

    Lift to 2.3.0

--- ExcelMvc/ExcelMvc/Functions/ExcelFunctionHost.cs
+++ ExcelMvc/ExcelMvc/Functions/ExcelFunctionHost.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using Range = Microsoft.Office.Interop.Excel.Range;
 
 namespace ExcelMvc.Functions
