2b12e6f134a2b37789dddf1fa358fb4a78ca75e9
Peter Gu <Pete.x.gu@gmail.com>
2024-08-19 14:37:15 +1000

    Parked RabbitMQ.Test

---
+++ Examples/RabbitMQ.Test/RabbitMQReceiver/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiver
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+                await Task.CompletedTask;
+            };
+            await channel.BasicConsumeAsync(queue: "hello",
+                                 autoAck: true,
+                                 consumerTag : "",
+                                 noLocal : false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Examples/RabbitMQ.Test/RabbitMQSender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQSender
+{
+    public static class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            while (true)
+            {
+                var message = $"{DateTime.Now:O}";
+                var body = Encoding.UTF8.GetBytes(message);
+                await channel.BasicPublishAsync(exchange: string.Empty,
+                                     routingKey: "hello",
+                                     //basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Sent {message}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
