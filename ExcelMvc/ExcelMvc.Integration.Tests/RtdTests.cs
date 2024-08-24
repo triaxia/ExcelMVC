8c414a4bbcce93fd3587d3a1c07e70e49e9cc291
Peter Gu <Pete.x.gu@gmail.com>
2024-08-24 12:01:03 +1000

    Added Integration testings

---
+++ ExcelMvc/ExcelMvc.Integration.Tests/RtdTests.cs
@@ -0,0 +1,35 @@
+﻿using Function.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelMvc.Integration.Tests
+{
+    [TestClass]
+    public class RtdTests
+    {
+        [Function(Name = "uTimer")]
+        public static object uTimer(string name)
+        {
+            return FunctionHost.Instance.Rtd<TimerServer>(() => new TimerServer(), "", name);
+        }
+
+        [TestMethod]
+        public void uTimer()
+        {
+            using (var excel = new ExcelLoader())
+            {
+                var items = new List<string>();
+                var start = DateTime.UtcNow;
+                while ((DateTime.UtcNow - start).TotalSeconds < 10)
+                {
+                    var result = (string)excel.Application.Run("uTimer", "test");
+                    items.Add(result);
+                }
+                var distinct = items.Distinct().Count();
+                Assert.IsTrue(distinct >= 10 && distinct <= 11);
+            }
+        }
+    }
+}
---
+++ ExcelMvc/ExcelMvc.Integration.Tests/TimerServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Function.Interfaces;
+
+namespace ExcelMvc.Integration.Tests
+{
+    public class TimerServer : IRtdServerImpl
+    {
+        public event EventHandler<RtdServerUpdatedEventArgs> Updated;
+        public readonly ConcurrentDictionary<int, RtdTopic> Topics
+            = new ConcurrentDictionary<int, RtdTopic>();
+
+        private Timer Timer { get; set; }
+
+        public int Start()
+        {
+            Timer = new Timer(TimerElapsed, null, 1000, 1000);
+            FunctionHost.Instance.RaisePosted(this, new MessageEventArgs("Started"));
+            return 1;
+        }
+
+        public void Stop()
+        {
+            FunctionHost.Instance.RaisePosted(this, new MessageEventArgs("Stopped"));
+            Timer.Dispose();
+            Topics.Clear();
+        }
+
+        public int Heartbeat()
+        {
+            return 1;
+        }
+
+        public object Connect(int topicId, string[] args)
+        {
+            FunctionHost.Instance.RaisePosted(this, new MessageEventArgs($"{topicId} connected"));
+            Topics[topicId] = new RtdTopic(args, DateTime.Now);
+            return Format(Topics[topicId]);
+        }
+
+        public void Disconnect(int topicId)
+        {
+            FunctionHost.Instance.RaisePosted(this, new MessageEventArgs($"{topicId} disconnected"));
+            Topics.TryRemove(topicId, out var _);
+        }
+
+        public object[,] GetTopicValues()
+        {
+            var snapshot = Topics.ToArray();
+            var values = new object[2, snapshot.Length];
+            for (int i = 0; i < snapshot.Length; i++)
+            {
+                values[0, i] = snapshot[i].Key;
+                values[1, i] = Format(snapshot[i].Value);
+            }
+            return values;
+        }
+
+        private void TimerElapsed(object _)
+        {
+            FunctionHost.Instance.RaisePosted(this, new MessageEventArgs("Ticked"));
+            var now = DateTime.Now;
+            foreach (var pair in Topics.ToArray())
+                pair.Value.Value = now;
+            Updated?.Invoke(this, new RtdServerUpdatedEventArgs(this, Topics.Values));
+        }
+
+        private static object Format(RtdTopic topic) => $"{topic}";
+    }
+}
