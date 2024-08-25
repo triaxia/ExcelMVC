294073c59e6ece12fb3981bdcc49cff38aaf8360
Peter Gu <Pete.x.gu@gmail.com>
2024-08-25 19:02:37 +1000

    Added Integration testings

--- ExcelMvc/ExcelMvc.Integration.Tests/ExcelLoader.cs
+++ ExcelMvc/ExcelMvc.Integration.Tests/ExcelLoader.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Excel;
 
 namespace ExcelMvc.Integration.Tests
 {
     public class ExcelLoader : IDisposable
     {
+        [DllImport("user32.dll")]
+        static extern int GetWindowThreadProcessId(int hWnd, out int lpdwProcessId);
+
         private static string AddInName
             => Path.Combine(Path.GetDirectoryName(typeof(BoolTests).Assembly.Location),
                  "ExcelMvc.Tests64.xll");
 
-        public Application Application { get; private set; }
-        public AddIn AddIn { get; private set; }
-       
+        public Application Application { get; }
+        public AddIn AddIn { get; }
+        public int ProcessId { get; }
 
-        public ExcelLoader() 
+        public ExcelLoader()
         {
             Application = new Application { Visible = false };
             var book = Application.Workbooks.Add();
             AddIn = Application.AddIns.Add(AddInName);
             AddIn.Installed = true;
+            GetWindowThreadProcessId(Application.Hwnd, out var id);
+            ProcessId = id;
         }
 
         public void Dispose()
@@ -30,10 +36,10 @@ namespace ExcelMvc.Integration.Tests
                 AddIn.Installed = false;
                 Application.Quit();
             }
-            catch
+            catch { }
+            finally
             {
-                foreach (var excel in Process.GetProcessesByName("Excel"))
-                    excel.Kill();
+                Process.GetProcessById(ProcessId)?.Kill();
             }
         }
     }
