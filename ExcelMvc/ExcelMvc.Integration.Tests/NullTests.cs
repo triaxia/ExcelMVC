c50c7f8d2595ea2ed6c23ae659275ebbe4e96e93
Peter Gu <Pete.x.gu@gmail.com>
2024-08-27 15:30:48 +1000

    #NUM for empty array or null outgoing

---
+++ ExcelMvc/ExcelMvc.Integration.Tests/NullTests.cs
@@ -0,0 +1,58 @@
+﻿using Function.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelMvc.Integration.Tests
+{
+    [TestClass]
+    public class NullTests
+    {
+        [Function()]
+        public static object[] uEmptyArray([Argument(Name = "[v1]")] object v1 = null)
+        {
+            return new object[] { };
+        }
+
+        [TestMethod]
+        public void uEmptyArray()
+        {
+            using (var excel = new ExcelLoader())
+            {
+                var result = (object)excel.Application.Run("uEmptyArray");
+                Assert.AreEqual(FunctionHost.Instance.ErrorNumbers[FunctionHost.Instance.ErrorNum], result);
+            }
+        }
+
+        [Function()]
+        public static object[] uNullArray([Argument(Name = "[v1]")] object v1 = null)
+        {
+            return null;
+        }
+        
+        [TestMethod]
+        public void uNullArray()
+        {
+            using (var excel = new ExcelLoader())
+            {
+                var result = (object)excel.Application.Run("uNullArray");
+                Assert.AreEqual(FunctionHost.Instance.ErrorNumbers[FunctionHost.Instance.ErrorNum], result);
+            }
+        }
+
+        [Function()]
+        public static object uNullValue([Argument(Name = "[v1]")] object v1 = null)
+        {
+            return null;
+        }
+
+        [TestMethod]
+        public void uNullValue()
+        {
+            using (var excel = new ExcelLoader())
+            {
+                var result = (object)excel.Application.Run("uNullValue");
+                Assert.AreEqual(FunctionHost.Instance.ErrorNumbers[FunctionHost.Instance.ErrorNum], result);
+            }
+        }
+
+    }
+}
