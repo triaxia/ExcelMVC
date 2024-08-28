af9ed5144cc7f0d6f93c8766169f6214cc24d166
Peter Gu <Pete.x.gu@gmail.com>
2024-08-28 14:58:05 +1000

    more tests for default arguments

--- ExcelMvc/ExcelMvc.Integration.Tests/DefaultTests.cs
+++ ExcelMvc/ExcelMvc.Integration.Tests/DefaultTests.cs
@@ -1,5 +1,6 @@
 ﻿using Function.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ExcelMvc.Integration.Tests
 {
@@ -9,14 +10,16 @@ namespace ExcelMvc.Integration.Tests
         private const string DefaultString = "gu";
         private const int DefaultInt = int.MinValue / 2;
         private const double DefaultDouble = 123.456;
+        private static DateTime ?DefaultDate = null;
 
         [Function()]
         public static string uDefault(
             [Argument(Name = "[v1]")] string v1 = DefaultString,
             [Argument(Name = "[v2]")] int? v2 = DefaultInt,
-            [Argument(Name = "[v3]")] double? v3 = DefaultDouble)
+            [Argument(Name = "[v3]")] double? v3 = DefaultDouble,
+            [Argument(Name = "[v4]")] DateTime? v4 = null)
         {
-            return $"{v1}|{v2}|{v3}";
+            return $"{v1}|{v2}|{v3}|{v4}";
         }
 
         [TestMethod]
@@ -25,10 +28,11 @@ namespace ExcelMvc.Integration.Tests
             using (var excel = new ExcelLoader())
             {
                 var result = (string)excel.Application.Run("uDefault");
-                Assert.AreEqual($"{DefaultString}|{DefaultInt}|{DefaultDouble}", result);
+                Assert.AreEqual($"{DefaultString}|{DefaultInt}|{DefaultDouble}|{DefaultDate}", result);
 
-                result = (string)excel.Application.Run("uDefault", "a", 1, 2);
-                Assert.AreEqual($"a|1|2", result);
+                var today = DateTime.SpecifyKind(DateTime.Today, DateTimeKind.Unspecified);
+                result = (string)excel.Application.Run("uDefault", "a", 1, 2, today);
+                Assert.AreEqual($"a|1|2|{today}", result);
             }
         }
     }
